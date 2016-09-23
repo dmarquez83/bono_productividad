ec18a1b13f620412f3cd6de885b1d35b5ea85cea
marquezdigna83 <3791852dig>
2016-09-23 16:00:21 -0400

    menu

--- resources/views/partials/home/header.blade.php
+++ resources/views/partials/home/header.blade.php
@@ -308,7 +308,7 @@
                         <li class="dropdown dropdown-user">
                             <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                                 <img alt="" class="img-circle" src="{{ asset('img/profile/avatar.png')}}" />
-                                <span class="username username-hide-on-mobile"> Nick </span>
+                                <span class="username username-hide-on-mobile"> {{ \Auth::user()->username }} </span>
                                 <i class="fa fa-angle-down"></i>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-default">
