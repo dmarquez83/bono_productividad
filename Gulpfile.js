ede1878aa4dfd3c028d9207ad02bd0b012fabe49
marquezdigna83 <3791852dig>
2016-11-09 15:59:19 -0400

    agregando alertify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,45 @@
 var  elixir = require('laravel-elixir');
-/*var Promise = require('es6-promise').Promise;*/
+var gulp = require('gulp');/*var Promise = require('es6-promise').Promise;*/
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+
+var dest = {
+    css: 'public/dist/css/',
+    js: 'public/dist/js/',
+    images: 'public/dist/images/',
+    views: 'public/dist/views'
+};
+
+var bowerCss = [
+    'bower_components/AlertifyJS/build/css/alertify.css',
+    'bower_components/AlertifyJS/build/css/themes/semantic.css'
+];
+
+gulp.task('componentsCss', function() {
+    return gulp.src(bowerCss)
+        .pipe(autoprefixer('last 15 version'))
+        .pipe(minifycss())
+        .pipe(concat('components.min.css'))
+        .pipe(gulp.dest(dest.css));
+});
+
+var bowerJs = [
+    'bower_components/AlertifyJS/build/alertify.js'
+];
+
+gulp.task('componentsJs', function() {
+    return gulp.src(bowerJs)
+        .pipe(concat('components.min.js'))
+        .pipe(gulp.dest(dest.js));
+});
+
+gulp.task('componentsJs2', function() {
+    return gulp.src(bowerJs)
+        .pipe(concat('components.min.js'))
+        .pipe(gulp.dest(dest.js));
+});
+
 elixir(function(mix) {
     mix.sass('app.scss')
         .styles([
@@ -14,7 +54,12 @@ elixir(function(mix) {
         //.scripts('resources/assets/js/admin/app.js', 'public/assets/js/admin/app.js');
 
     mix.scripts('./bower_components/angular/angular.min.js', 'public/assets/js/angular.min.js');
+    mix.scripts('./bower_components/AlertifyJS/build/alertify.js', 'public/assets/js/alertify.js');
+    mix.styles('./bower_components/AlertifyJS/build/css/alertify.css', 'public/assets/css/alertify.css');
+    mix.styles('./bower_components/AlertifyJS/build/css/themes/semantic.css', 'public/assets/css/themes/semantic.css');
     mix.browserSync({proxy: 'localhost:8000'});
+//  mix.task('default', 'componentsJs' );
+
 });
 
-
+//gulp.task('default', ['componentsCss','componentsJs']); buscar como utilizarlo con con el copy
--- resources/views/modules/admin/groups/user_group.php
+++ resources/views/modules/admin/groups/user_group.php
@@ -1,7 +1,6 @@
 <!-- declare our angular app and controller -->
-<body class="container"  ng-controller="groupUserController">
-<form >
-	<div class="row">
+<form ng-submit="to_assign()" ng-controller="groupUserController">
+    <div class="row"  >
 	  <p class="text-center" ng-show="loading"><span class="fa fa-spinner fa-5x fa-spin"></span></p>
 	  <!-- lista de usuario-->
 	  <div class="col-md-6 col-sm-6">
@@ -14,7 +13,7 @@
 				  </div>
 				  <div class="actions">
 					  <div class="btn-group">
-						  <button type="button" class="btn btn-circle grey-mint" ng-click="isChecked_user_all()" >Seleccionar Todos</button>
+						  <button type="button" class="btn btn-circle grey-mint" ng-click="isChecked_user_all()" >Seleccionar Todos </button>
 					  </div>
 					  <a class="btn btn-circle btn-icon-only btn-default fullscreen" href="javascript:;" data-original-title="" title=""> </a>
 				  </div>
@@ -42,12 +41,12 @@
 						  <!-- END START TASK LIST -->
 					  </div>
 				  </div>
-				 <div class="task-footer">
+                  <!-- <div class="task-footer">
                     <div class="btn-arrow-link pull-right">
                         <a href=""  ng-click="to_assign()" >Asignar a</a>
                         <i class="icon-arrow-right"></i>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -85,85 +84,85 @@
 						  <!-- END START TASK LIST -->
 					  </div>
 				  </div>
-				  <div class="task-footer">
-					  <div class="btn-arrow-link pull-right">
-
-					  </div>
-				  </div>
 			  </div>
 		  </div>
 	  </div>
 	  <!-- fin lista de grupos-->
-	</div>   <!-- fin row 1-->
+    </div>   <!-- fin row 1-->
+
+    <div class="text-center" ng-controller="accessUsersController" >
+        <input type="hidden"  ng-init="menu_modules = 5">
+        <button type="submit" class="btn blue" ng-disabled="acc_insert" >Guardar</button>
+    </div>
 </form>
-<div class="row">
+    <div class="row"  ng-controller="groupUserController">
 	<!-- usuarios asignados a grupos -->
-	<div class="col-lg-12">
-		<div class="portlet light portlet-fit bordered">
+        <div class="col-lg-12">
+            <div class="portlet light portlet-fit bordered">
 
-			<div class="portlet-body">
-				<div class="mt-element-list">
-				  <a class="list-toggle-container font-white" data-toggle="collapse" href="#lista-grupo" aria-expanded="false">
-					  <div class="mt-list-head list-todo green">
-						  <div class="list-head-title-container">
-							  <h3 class="list-toggle-title bold list-title">Usuarios Asignados a Grupos</h3>
-							  <div class="list-head-count">
-								  <div class="list-head-count-item">
-									  <i class="fa fa-user"></i> {{ group_users.length }}</div>
-								  <div class="list-head-count-item">
-									  <i class="fa fa-users"></i> {{ users.length }}</div>
-							  </div>
-						  </div>
-						  <div class="list-count pull-right green-meadow">
-							<i class="fa fa-plus"></i>
-						  </div>
+                <div class="portlet-body">
+                    <div class="mt-element-list">
+                      <a class="list-toggle-container font-white" data-toggle="collapse" href="#lista-grupo" aria-expanded="false">
+                          <div class="mt-list-head list-todo green">
+                              <div class="list-head-title-container">
+                                  <h3 class="list-toggle-title bold list-title">Usuarios Asignados a Grupos</h3>
+                                  <div class="list-head-count">
+                                      <div class="list-head-count-item">
+                                          <i class="fa fa-user"></i> {{ group_users.length }}</div>
+                                      <div class="list-head-count-item">
+                                          <i class="fa fa-users"></i> {{ users.length }}</div>
+                                  </div>
+                              </div>
+                              <div class="list-count pull-right green-meadow">
+                                <i class="fa fa-plus"></i>
+                              </div>
 
-					  </div>
-					</a>
-					<div class="mt-list-container list-todo task-list panel-collapse collapse" id="lista-grupo">
-						<div class="list-todo-line red"></div>
-						<ul>
-							<li class="mt-list-item" ng-hide="loading" ng-repeat="group_user in group_users track by $index" >
-								<div class="list-todo-icon bg-white font-green-meadow">
-									<i class="fa fa-users"></i>
-								</div>
-								<div class="list-todo-item green-meadow">
-									<a class="list-toggle-container font-white" data-toggle="collapse" href="#task-{{$index+1}}-2" aria-expanded="false">
-										<div class="list-toggle done uppercase">
-											<div class="list-toggle-title bold">{{ group_user.group.name }}</div>
-											<div class="badge badge-default pull-right bold">{{ group_user.user.length }}</div>
-										</div>
-									</a>
-									<div class="task-list panel-collapse collapse" id="task-{{$index+1}}-2">
-										<ul>
-											<li class="task-list-item done"  ng-hide="loading" ng-repeat="user_data in group_user.user ">
-												<div class="task-icon" ng-hide="loading">
-													<img src="/img/profile/{{user_data.profile[0].profile.avatar}}"  class="img-circle" alt="" width="50px" height="52px"  >
-												</div>
-												<div class="task-status">
-													<a class="done" href="" ng-click="delete_user_group(user_data.users.id,group_user.group.id)">
-														<i class="fa fa-close"></i>
-													</a>
-												</div>
-												<div class="task-content">
-													<h4 class="uppercase bold">
-														<a href="javascript:;">{{  user_data.users.username }}   </a>
-													</h4>
-													<p>{{  user_data.profile[0].profile.name }}</p>
-												</div>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                          </div>
+                        </a>
+                        <div class="mt-list-container list-todo task-list panel-collapse collapse" id="lista-grupo">
+                            <div class="list-todo-line red"></div>
+                            <ul>
+                                <li class="mt-list-item" ng-hide="loading" ng-repeat="group_user in group_users track by $index" >
+                                    <div class="list-todo-icon bg-white font-green-meadow">
+                                        <i class="fa fa-users"></i>
+                                    </div>
+                                    <div class="list-todo-item green-meadow">
+                                        <a class="list-toggle-container font-white" data-toggle="collapse" href="#task-{{$index+1}}-2" aria-expanded="false">
+                                            <div class="list-toggle done uppercase">
+                                                <div class="list-toggle-title bold">{{ group_user.group.name }}</div>
+                                                <div class="badge badge-default pull-right bold">{{ group_user.user.length }}</div>
+                                            </div>
+                                        </a>
+                                        <div class="task-list panel-collapse collapse" id="task-{{$index+1}}-2">
+                                            <ul>
+                                                <li class="task-list-item done"  ng-hide="loading" ng-repeat="user_data in group_user.user ">
+                                                    <div class="task-icon" ng-hide="loading">
+                                                        <img src="/img/profile/{{user_data.profile[0].profile.avatar}}"  class="img-circle" alt="" width="50px" height="52px"  >
+                                                    </div>
+                                                    <div class="task-status">
+                                                        <a class="done" href="" ng-click="delete_user_group(user_data.users.id,group_user.group.id)">
+                                                            <i class="fa fa-close"></i>
+                                                        </a>
+                                                    </div>
+                                                    <div class="task-content">
+                                                        <h4 class="uppercase bold">
+                                                            <a href="javascript:;">{{  user_data.users.username }}   </a>
+                                                        </h4>
+                                                        <p>{{  user_data.profile[0].profile.name }}</p>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 	<!-- fin de usuarios asignados a grupos -->
-</div>
-</body>
+    </div> 	<!-- fin de row2 -->
+
 
 
